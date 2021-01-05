9d112174f2212a22608199fec543b2fceb4a4225
Strixis <nivem-strixis@mail.ru>
2021-01-05 14:54:21 +0300

    Сделан footer

--- src/components/ComponentFooter.vue
+++ src/components/ComponentFooter.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    This is footer
-  </div>
+  <footer class="footer">
+    <span class="footer_text">&copy;Strixis, 2020-{{year}}. All rights reserved.</span>
+  </footer>
 </template>
+
+<script>
+export default {
+  computed: {
+    year: () => {
+      return new Date().getFullYear()
+    }
+  }
+}
+</script>
